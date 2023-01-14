aeccc8a8ebd9f3ad6a86b39931347326d1517605
CorinJG <cogilmore@gmail.com>
2023-01-14 01:04:28 +0000

    day3 complete

---
+++ src/day2/day2.rs
@@ -0,0 +1,49 @@
+static INPUT: &'static str = include_str!("input.txt");
+
+fn score_map1(s: &str) -> u32 {
+    match s {
+        "A X" => 4,
+        "A Y" => 8,
+        "A Z" => 3,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 7,
+        "C Y" => 2,
+        "C Z" => 6,
+        _ => panic!("invalid input"),
+    }
+}
+
+fn score_map2(s: &str) -> u32 {
+    match s {
+        "A X" => score_map1("A Z"),
+        "A Y" => score_map1("A X"),
+        "A Z" => score_map1("A Y"),
+        "B X" => score_map1("B X"),
+        "B Y" => score_map1("B Y"),
+        "B Z" => score_map1("B Z"),
+        "C X" => score_map1("C Y"),
+        "C Y" => score_map1("C Z"),
+        "C Z" => score_map1("C X"),
+        _ => panic!("invalid input"),
+    }
+}
+
+fn main() {
+    let score1 = INPUT
+        .trim()
+        .lines()
+        .map(|round| score_map1(round))
+        .sum::<u32>();
+
+    println!("part 1 score: {}", score1);
+
+    let score2 = INPUT
+        .trim()
+        .lines()
+        .map(|round| score_map2(round))
+        .sum::<u32>();
+
+    println!("part 2 score: {}", score2);
+}
---
+++ src/day3/day3.rs
@@ -0,0 +1,60 @@
+static INPUT: &'static str = include_str!("input.txt");
+
+/// Returns char common to first and second half of a str slice of even length
+fn common_item(s: &str) -> char {
+    let (left, right) = s.split_at(s.len() / 2);
+    assert_eq!(left.len(), right.len(), "invalid input - odd number of items in a bag");
+    for c in left.chars() {
+        if right.contains(c) {
+            return c
+        }
+    } 
+    panic!("invalid input - no item present in both compartments");
+}
+
+fn priority(c: char) -> u32 {
+    match c {
+        c @ 'a'..='z' => (c as u8 - 96) as u32, // a-z is ASCII 97-122
+        c @ 'A'..='Z' => (c as u8 - 38) as u32, // A-Z is ASCII 65-90
+        _ => panic!("invalid input - unexpected character"),
+    }
+}
+
+/// Return the item which is common to all 3 elves' bags
+fn determine_badge(a: &str, b: &str, c: &str) -> char {
+    for i in a.chars() {
+        if b.contains(i) && c.contains(i) {
+            return i
+        }
+    }
+    panic!("invalid input - no item common to all 3 bags");
+}
+
+fn main() {
+    let result1 = INPUT
+        .trim()
+        .lines()
+        .map(|items| common_item(items))
+        .map(|common_item| priority(common_item))
+        .sum::<u32>();
+
+    println!("answer1: {}", result1);
+
+    let result2 = INPUT
+        .trim()
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks_exact(3)
+        .map(|bag_triplet| {
+            // necessary to eliminate 'refutable pattern' error
+            if let [a,b,c] = bag_triplet {
+                determine_badge(a, b, c)
+            } else {
+                panic!("invalid inpuit - number of bags not a multiple of 3")
+            }
+        })
+        .map(|badge| priority(badge))
+        .sum::<u32>();
+
+        println!("answer2: {}", result2);
+}
