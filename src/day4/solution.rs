ad9d8e4e7db7d3ee94aacd237c87e26d5647f939
CorinJG <cogilmore@gmail.com>
2023-01-14 21:14:12 +0000

    day5

---
+++ src/day4/solution.rs
@@ -0,0 +1,44 @@
+static INPUT: &'static str = include_str!("input.txt");
+
+fn main() {
+    let range_bounds = INPUT
+        .trim()
+        .lines()
+        .map(|ranges| {
+            let (range1, range2) = ranges
+                .split_once(",")
+                .expect("multiple ranges on line");
+            let (low1, high1) = range1
+                .split_once("-")
+                .expect("invalid range");
+            let (low2, high2) = range2
+                .split_once("-")
+                .expect("invalid range");
+            let low1: u32 = low1.parse().expect("non-integral range bound");
+            let high1: u32 = high1.parse().expect("non-integral range bound");
+            let low2: u32 = low2.parse().expect("non-integral range bound");
+            let high2: u32 = high2.parse().expect("non-integral range bound");
+            (low1, high1, low2, high2)
+        })
+        .collect::<Vec<_>>();
+
+    let solution1 = range_bounds
+        .iter()
+        .filter(|(low1, high1, low2, high2)| {
+            (low1 <= low2 && high1 >= high2) || (low2 <= low1 && high2 >= high1)
+        })
+        .count();
+
+    println!("part1: {}", solution1);
+
+    let solution2 = range_bounds
+        .iter()
+        .filter(|(low1, high1, low2, high2)| {
+            (low2 >= low1 && low2 <= high1) || 
+            (high2 >= low1 && high2 <= high1) ||
+            (low1 >= low2 && high1 <= high2)
+        })
+        .count();
+    
+    println!("part2: {}", solution2);
+}
---
+++ src/day5/solution.rs
@@ -0,0 +1,68 @@
+static INPUT: &'static str = include_str!("input.txt");
+
+fn move_crates_9000(stacks: &mut Vec<Vec<char>>, num: u32, src: usize, dest: usize) {
+    for _ in 0..num {
+        // input stack indexes begin at 1 so must subtract 1
+        let popped_crate = stacks[src-1]
+            .pop()
+            .expect("instruction invalid - stack has insufficient crates");
+        stacks[dest-1].push(
+            popped_crate
+        );
+    }
+}
+
+fn move_crates_9001(stacks: &mut Vec<Vec<char>>, num: u32, src: usize, dest: usize) {
+    let mut popped_crates: Vec<char> = Vec::new();
+    for _ in 0..num {
+        popped_crates.push(
+            stacks[src-1].pop().expect("instruction invalid - stack has insufficient crates")
+        );
+    }
+    // empty the stack of popped crates, popping onto dest stack
+    while let Some(c) = popped_crates.pop() {
+        stacks[dest-1].push(c);
+    }
+}
+
+fn main() {
+    let mut stacks: Vec<Vec<char>> = Vec::with_capacity(9);
+    stacks.push(['D','B','J','V'].to_vec());
+    stacks.push(['P','V','B','W','R','D','F'].to_vec());
+    stacks.push(['R','G','F','L','D','C','W','Q'].to_vec());
+    stacks.push(['W','J','P','M','L','N','D','B'].to_vec());
+    stacks.push(['H','N','B','P','C','S','Q'].to_vec());
+    stacks.push(['R','D','B','S','N','G'].to_vec());
+    stacks.push(['Z','B','P','M','Q','F','S','H'].to_vec());
+    stacks.push(['W','L','F'].to_vec());
+    stacks.push(['S','V','F','M','R'].to_vec());
+
+    // copy of initial crate state for part 2
+    let mut stacks2 = stacks.clone();
+
+    for instruction in INPUT.trim().lines() {
+        let instruction_vec: Vec<&str> = instruction.split(" ").collect();
+        if let [_, num, _, src, _, dest] = instruction_vec.as_slice() {
+            let num: u32 = num.parse().expect("number of crates isn't a number");
+            let src: usize = src.parse().expect("source stack isn't a number");
+            let dest: usize = dest.parse().expect("dest stack isn't a number");
+            move_crates_9000(&mut stacks, num, src, dest);
+            move_crates_9001(&mut stacks2, num, src, dest);
+        } else {
+            panic!("instruction of unexpected format");
+        }
+    }
+    
+    let solution1 = stacks
+        .iter()
+        .filter_map(|stack| stack.last())
+        .collect::<String>();
+    println!("part 1: {}", solution1);
+
+    let solution2 = stacks2
+        .iter()
+        .filter_map(|stack| stack.last())
+        .collect::<String>();
+    println!("part 2: {}", solution2);
+
+}
